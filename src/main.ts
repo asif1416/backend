3784dc731171d40e0813b55c007086c9d7fe727e
asif1416 <ashrafulasif260@gmail.com>
2025-02-09 22:41:22 +0600

    connecting

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   app.useGlobalGuards(new AuthGuard(jwtService, reflector));
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: 'https://calinary-odissey.vercel.app/',
     credentials: true,
   });
   app.use(helmet());
