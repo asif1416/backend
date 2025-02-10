36ddf9b0c3d6e49d24b236fe5dd5307504b3bac7
asif1416 <ashrafulasif260@gmail.com>
2025-02-10 16:04:07 +0600

    connecting

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,6 +31,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     MailerModule.forRoot({
       transport: {
         host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT),
+        secure: false,
         auth: {
           user: process.env.SMTP_USER,
           pass: process.env.SMTP_PASSWORD,
