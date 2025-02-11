2ef5f24d29d965ce42e7f5ba5dde699de725e7e6
Asif259 <ashrafulasif259@gmail.com>
2025-02-11 12:43:28 +0600

    connecting

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,12 +32,11 @@ config();
     }),
     MailerModule.forRoot({
       transport: {
-        host: process.env.SMTP_HOST,
-        port: Number(process.env.SMTP_PORT),
+        host: 'smtp.gmail.com',
         secure: false,
         auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASSWORD,
+          user: 'ashrafulasif260@gmail.com',
+          pass: 'dmpd ekrn qjwg eolz',
         },
       },
     }),
